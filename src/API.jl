4ca63eebad4615d836a7effb445664378528d041
Alan Enzo <gamerenzo03@gmail.com>
2024-06-27 13:11:40 -0300

    integração

--- src/API.jl
+++ src/API.jl
@@ -1,6 +1,7 @@
 #module API
 using Genie
-
+include("bancodedados.jl")
+include("Controllers.jl")
 
 route(deleteuser, "/delete/:id", method = "DELETE")
 route(ControllerCreateUser, "/criar", method = "POST")
